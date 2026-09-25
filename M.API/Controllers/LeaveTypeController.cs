d67f80474f125e63dd7ec797b302c749b4b62e87
dang <huynhhoangdang2005@ngdang2005gmail.com>
2026-09-25 10:28:45 +0700

    dang

--- M.API/Controllers/LeaveTypeController.cs
+++ M.API/Controllers/LeaveTypeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using M.Contract.Services.Interface;
+using M.Core.Base;
+using M.Core.Store;
+using Microsoft.AspNetCore.Mvc;
+using ModelViews.LeaveTypeModelView;
 
 namespace M.API.Controllers
 {
@@ -6,5 +10,85 @@ namespace M.API.Controllers
     [ApiController]
     public class LeaveTypeController : ControllerBase
     {
+        private readonly ILeaveTypeService _leaveTypeService;
+
+        public LeaveTypeController(ILeaveTypeService leaveTypeService)
+        {
+            _leaveTypeService = leaveTypeService;
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAll(int pageNumber = 1, int pageSize = 5)
+        {
+            BasePaginatedList<LeaveTypeResponseModelView> result =
+                await _leaveTypeService.GetAllAsync(pageNumber, pageSize);
+
+            return Ok(new BaseResponse<BasePaginatedList<LeaveTypeResponseModelView>>(
+                statusCode: StatusCodeHelper.OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: result
+            ));
+        }
+
+        [HttpGet("get-by-id/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            LeaveTypeResponseModelView result =
+                await _leaveTypeService.GetByIdAsync(id);
+
+            return Ok(new BaseResponse<LeaveTypeResponseModelView>(
+                statusCode: StatusCodeHelper.OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: result
+            ));
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody] CreateLeaveTypeModelView model)
+        {
+            await _leaveTypeService.CreateAsync(model);
+
+            return Ok(new BaseResponse<string>(
+                statusCode: StatusCodeHelper.OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: "Leave type created successfully!"
+            ));
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update([FromBody] UpdateLeaveTypeModelView model)
+        {
+            await _leaveTypeService.UpdateAsync(model);
+
+            return Ok(new BaseResponse<string>(
+                statusCode: StatusCodeHelper.OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: "Leave type updated successfully!"
+            ));
+        }
+
+        [HttpDelete("soft-delete/{id}")]
+        public async Task<IActionResult> SoftDelete(Guid id)
+        {
+            await _leaveTypeService.SoftDeleteAsync(id);
+
+            return Ok(new BaseResponse<string>(
+                statusCode: StatusCodeHelper.OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: "Leave type deleted successfully!"
+            ));
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _leaveTypeService.DeleteAsync(id);
+
+            return Ok(new BaseResponse<string>(
+                statusCode: StatusCodeHelper.OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: "Leave type permanently deleted successfully!"
+            ));
+        }
     }
 }
